5c9ac76c32f409d245ee5230fde91facf0ce81bd
Ronak Shah <ronakshah725@gmail.com>
2016-05-19 23:32:08 -0500

    Added dup remove, and remove midnode given only node

--- LinkedList.java
+++ LinkedList.java
@@ -1,47 +1,84 @@
-public class LinkedList{
-	
+import java.util.*;
+
+public class LinkedList {
+
 	private Node head;
 
 	/*
-		Append a new Node to the tail	 
-	*/
+	 * Head Getter
+	 */
 
-	void appendToTail(int d){
+	public Node getHead() {
+
+		return head;
+	}
+
+	/*
+	 * Head Setter
+	 */
+
+	public void setHead(Node head) {
+
+		this.head = head;
+	}
+
+	class Node {
+
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [data=" + data + "]";
+		}
+	}
+
+	/**************** LIST HELPERS *************************/
+
+	/*
+	 * Append a new Node to the tail
+	 */
+
+	void appendToTail(int d) {
 		Node n = new Node(d);
-		if(head == null){
+		if (head == null) {
 			head = n;
 			return;
 		}
 
 		Node current = head;
-		while(current.next!=null){
+		while (current.next != null) {
 			current = current.next;
 		}
-		current.next  = n;
-
+		current.next = n;
 	}
 
 	/*
-		Create a new LinkedList from given int array
+	 * Create a new LinkedList from given int array
 	 */
 
-	void newNodeFromArray(int[] arr){
+	void newNodeFromArray(int[] arr) {
 
 		this.head = new Node(arr[0]);
 		Node n = this.head;
-		for (int i=1; i<arr.length; i++){
+		for (int i = 1; i < arr.length; i++) {
 			n.next = new Node(arr[i]);
 			n = n.next;
 		}
 	}
 
 	/*
-		Append a new Node to the front or head
+	 * Append a new Node to the front or head
 	 */
 
-	void appendToFront(int data){
+	void appendToFront(int data) {
 		Node n = new Node(data);
-		if (head==null){
+		if (head == null) {
 			head = n;
 			return;
 		}
@@ -50,41 +87,60 @@ public class LinkedList{
 	}
 
 	/*
-		Print the LinkedList
+	 * Print the LinkedList
 	 */
 
-	void printList(){
+	void printList() {
 
 		System.out.print("LinkedList: ");
-		if(head == null){
+		if (head == null) {
 			System.out.println(" -> ");
 			return;
 		}
-		Node current  = head;
-		while(current != null){
+		Node current = head;
+		while (current != null) {
 			System.out.print(current.data + " -> ");
 			current = current.next;
 		}
 		System.out.println();
 	}
 
+	int length(Node curr) {
+
+		if (curr == null)
+			return 0;
+		return 1 + length(curr.next);
+	}
+
 	/*
-		Get the Middle Element
-		Trick: 
+	 * get the number of nodes in a linkedlist
 	 */
 
-	int getMid(){
-		if(head == null) return -1;
-		if(head.next == null) return head.data;
+	int length() {
+
+		return length(this.head);
+	}
+
+	/**************** LIST DS ALGO *************************/
+
+	/*
+	 * 1. Get the Middle Element Trick:
+	 */
+
+	int getMid() {
+		if (head == null)
+			return -1;
+		if (head.next == null)
+			return head.data;
 
 		Node fast = head;
 		Node slow = head;
-		while(fast.next!= null){
+		while (fast.next != null) {
 
-			if(fast.next.next!= null){
+			if (fast.next.next != null) {
 				fast = fast.next.next;
 				slow = slow.next;
-			}else{
+			} else {
 				fast = fast.next;
 			}
 		}
@@ -92,22 +148,23 @@ public class LinkedList{
 	}
 
 	/*
-		Deletes  first occurrences and returns the head
+	 * 2. Deletes first occurrences and returns the head Boundary: 1. Handle
+	 * delete from heads seperately
 	 */
-	
-	Node delete(int data){
 
-		//if the first node
-		if(head.data == data){
+	Node delete(int data) {
+
+		// if the first node
+		if (head.data == data) {
 			head = head.next;
 			return head;
 		}
-		//if the middle or last node
+		// if the middle or last node
 		Node prev = head;
 		Node current = head.next;
 
-		while(current != null){
-			if(current.data == data){
+		while (current != null) {
+			if (current.data == data) {
 				prev.next = current.next;
 				return head;
 			}
@@ -118,119 +175,106 @@ public class LinkedList{
 	}
 
 	/*
-	 * Delete all occurrences and returns the head
+	 * 3. Delete all occurrences and returns the head
 	 */
-	
-	void deleteAll(int data){
 
-		//Delete all first occurrences
-		while(head.data == data){
+	void deleteAll(int data) {
+
+		// Delete all first occurrences
+		while (head.data == data) {
 			head = head.next;
 		}
-		//if the middle or last node
+		// if the middle or last node
 		Node prev = head;
 		Node current = head;
-		
-		while(current != null){
-			if(current.data == data){
+
+		while (current != null) {
+			if (current.data == data) {
 				prev.next = current.next;
-				
+
 			}
 			prev = current;
 			current = current.next;
 		}
 	}
 
-
-	int length(Node curr){
-
-		if(curr==null)
-			return 0;
-		return 1+length(curr.next);
-
-	}
-
 	/*
-		get the number of nodes in a linkedlist
+	 * 4. reverse all Logic: 1. For each node n, store the next and the prev
+	 * node 2. reverse links for n and update Boundary: 1. One or element array
 	 */
+	void reverse() {
 
-	int length(){
-
-		return length(this.head);
-	}
-
-	//null
-	//1
-	//1 2
-	//1 2 3
-	//1 2 3 4
-	
-	/*
-	reverse a ll
-	Logic: 
-		1. For each node n, store the next and the prev node
-		2. reverse links for n and update 
-	 */
-	void reverse(){
-
-		if (head==null || (head!=null && head.next==null)) 
-			return;									// only 0 or 1 elements
-													
+		if (head == null || head.next == null)
+			return; // only 0 or 1 elements
 
 		Node n = head;
-		Node prev = null; 							//since previous to the first node 
-													//will be null in the reversed linkedList
-		
+		Node prev = null; // since previous to the first node will be null in the reversed linkedList
+
 		Node next = head;
 
-		while( n != null ){
+		while (n != null) {
 
-			next = n.next; 							// store the next node
-			n.next = prev; 							// change n's next to previos node
-			
-			prev = n;	   							// Reversing done now setup for next iter
+			next = n.next; // store the next node
+			n.next = prev; // change n's next to previos node
+
+			prev = n; // Reversing done now setup for next iter
 			n = next;
 		}
-		this.head = prev;							// Head in previous node
-
-	}
-
-
-	/*
-		Head Getter
-	 */
-
-	
-	public Node getHead() {
-		return head;
+		this.head = prev; // Head in previous node
 	}
 
 	/*
-		Head Setter
+	 * 5. Remove Duplicates Logic: 1. O(n) time, O(n) space, hashTable 2.
+	 * prev and n, move prev ahead only if found a non dup element, else, prev
 	 */
 
-	public void setHead(Node head) {
-		this.head = head;
-	}
+	// 1
+	// 1 1
+	// 1 2 1
+	// 1 2 1 4
+	void removeDups() {
 
+		Set<Integer> set = new HashSet<>();
+		Node n = head;
+		Node prev = head;
 
-	class Node{
-		
-		int data;
-		Node next;
+		while (n != null) {
+			if (!set.contains(n.data)) {
+				set.add(n.data);
+				prev = n;
 
-		Node(int data){
-			this.data = data;
-			this.next = null;
+			} else {
+				prev.next = n.next;
+			}
+			n = n.next;
+
 		}
 
-		@Override
-		public String toString() {
-			return "Node [data=" + data + "]";
+	}
+
+/*
+	6. Delete middle node, given the node
+		middle here means any except first or last
+		a -> n-> c -> d
+		input: n
+		output: nothing but new list becomes a->c->d
+		Logic: reference of n is given, so the a.next points to this reference, 
+		let  
+			n.data = n.next.data //store c's data in n's ,coz removing n
+			n.next (ie. c) = n.next.next;
+
+
+
+ */
+
+		void removeMidNode(Node n){
+			if(n == null || n.next == null || n == head) //cant be a null, or last or first node
+				return;
+			n.data = n.next.data;
+			n.next = n.next.next;
+
 		}
 
 
-	}
 
 }
-
--- TestLL.java
+++ TestLL.java
@@ -5,16 +5,16 @@ public class TestLL{
 		//Delete All
 		LinkedList ll = new LinkedList();
 		ll.newNodeFromArray(new int[]{1, 1, 1, 30, 11 });
+		println("\nDeleting All: 1");	
 		ll.printList();
-		println("Deleting All: 1");	
 		ll.deleteAll(1);
 		ll.printList();	
 
 		//Delete
 		LinkedList ld = new LinkedList();
 		ld.newNodeFromArray(new int[]{1, 1, 1, 30, 11 });
+		println("\nDeleting First: 30");	
 		ld.printList();
-		println("Deleting First: 30");	
 		ld.delete(30);
 		ld.printList();	
 
@@ -22,7 +22,7 @@ public class TestLL{
 		LinkedList lmid = new LinkedList();
 		lmid.newNodeFromArray(new int[]{1, 2, 43, 30, 11 });
 		lmid.printList();
-		println("Mid:" + lmid.getMid());	
+		println("\nMid:" + lmid.getMid());	
 
 		//Length
 		println("Length: " + lmid.length());
@@ -30,13 +30,27 @@ public class TestLL{
 		//Reverse
 		LinkedList lrev = new LinkedList();
 		lrev.newNodeFromArray(new int[]{1, 2, 3 });
-		println("Reversing.." );				
+		println("\nReversing.." );				
 		lrev.printList();
-		
 		lrev.reverse();
-		
 		lrev.printList();
 
+		//Remove Duplicates
+		LinkedList llremDups = new LinkedList();
+		llremDups.newNodeFromArray(new int[]{1, 2, 4, 4, 1, 2, 5, 6, 1});
+		println("\nRemoving duplicates" );				
+		llremDups.printList();
+		llremDups.removeDups();
+		llremDups.printList();
+
+		//DeleteMiddleNode given only that node
+		LinkedList llremMid = new LinkedList();
+		llremMid.newNodeFromArray(new int[]{1, 2, 4, 6, 1});
+		LinkedList.Node nDel = llremMid.getHead().next.next; //4
+		println("\nDeleteMiddleNode given only that node, node:" + nDel.data );				
+		llremMid.printList();
+		llremMid.removeMidNode(nDel);
+		llremMid.printList();
 		
 
 	}
